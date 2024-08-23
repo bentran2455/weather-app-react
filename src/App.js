6f272cc2185f47998dfb1b23a4e44e6a19109971
Ben Tran <letscontactben@gmail.com>
2024-08-23 16:12:13 +0700

    Weather app React

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+const api = {
+  key: "f94f77bda0edd4c3f48623fa906b23b4",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [searchCity, setSearchCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      if (!searchCity) return;
+      setLoading(true);
+      try {
+        const url = `${api.base}weather?q=${searchCity}&units=metric&APPID=${api.key}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        if (response.ok) {
+          setWeatherInfo(
+            `${data.name},${data.sys.country},${data.weather[0].description},${data.main.temp}`
+          );
+          setErrorMessage("");
+        } else {
+          setErrorMessage(data.message);
+        }
+      } catch (err) {
+        setErrorMessage(err.message);
+      }
+      setLoading(false);
+    };
+    fetchWeatherData();
+  }, [searchCity]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchCity(searchInput);
+  };
+  return (
+    <>
+      <form action="#" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="City"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {errorMessage ? (
+            <div style={{ color: "red" }}>{errorMessage}</div>
+          ) : (
+            <div>{weatherInfo}</div>
+          )}
+        </>
+      )}
+    </>
+  );
+}
+
+export default App;
